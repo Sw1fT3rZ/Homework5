88cbcdc9c525ca22dd82fd54b32a44792392387d
Sw1fT3rZ <130749643+Sw1fT3rZ@users.noreply.github.com>
2024-10-10 20:55:19 +0300

    Add files via upload
    Завдання 1 та 2 були виконані з урахуванням завдання 3

---
+++ Part1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Text;
+
+class Play : IDisposable
+{
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public string Genre { get; set; }
+    public int Year { get; set; }
+
+    public Play(string title, string author, string genre, int year)
+    {
+        Title = title;
+        Author = author;
+        Genre = genre;
+        Year = year;
+    }
+
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"Назва: {Title}, Автор: {Author}, Жанр: {Genre}, Рік: {Year}");
+    }
+
+    ~Play()
+    {
+        Console.WriteLine($"Деструктор викликаний для п'єси: {Title}");
+    }
+
+    public void Dispose()
+    {
+        Console.WriteLine($"Метод Dispose викликаний для п'єси: {Title}");
+        GC.SuppressFinalize(this);
+    }
+}
+
+class Part1
+{
+    static void Main()
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        Console.InputEncoding = Encoding.UTF8;
+
+        Console.WriteLine("=== Тестування класу «П'єса» ===");
+
+        using (Play play = new Play("Гамлет", "Вільям Шекспір", "Трагедія", 1601))
+        {
+            play.DisplayInfo();
+        }  
+        Console.WriteLine("П'єса більше не використовується.");
+    }
+}
---
+++ Part2.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Text;
+
+class Store : IDisposable
+{
+    public string StoreName { get; set; }
+    public string Address { get; set; }
+    public string StoreType { get; set; }
+
+    public Store(string storeName, string address, string storeType)
+    {
+        StoreName = storeName;
+        Address = address;
+        StoreType = storeType;
+    }
+
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"Назва магазину: {StoreName}, Адреса: {Address}, Тип: {StoreType}");
+    }
+
+    public void Dispose()
+    {
+        Console.WriteLine($"Метод Dispose викликаний для магазину: {StoreName}");
+        GC.SuppressFinalize(this);
+    }
+
+    ~Store()
+    {
+        Console.WriteLine($"Деструктор викликаний для магазину: {StoreName}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        Console.InputEncoding = Encoding.UTF8;
+
+        Console.WriteLine("=== Тестування класу «Магазин» ===");
+
+        using (Store store = new Store("Продукти", "вул. Вулична, 666", "Продовольчий"))
+        {
+            store.DisplayInfo();
+        }  
+        Console.WriteLine("Магазин більше не використовується.");
+    }
+}
